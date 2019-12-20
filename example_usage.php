39481339d88ea64d280fe80b9697077a7a412f15
Brooklyn-Forrest <45461267+Brooklyn-Forrest@users.noreply.github.com>
2019-12-20 09:07:19 -0600

    Create example_usage.php

---
+++ example_usage.php
@@ -0,0 +1,19 @@
+<?php
+
+class avm_backup_api
+{
+    private $authtoken;
+    protected $shell;
+
+    function init(){
+        $this->shell = new curl_handler();
+
+        $header = array("Content-Type: application/x-www-form-urlencoded");
+        $opts =array($req_mode="POST", $use_proxy="FALSE", $ssl="FALSE", $ignore_ssl_hostname="FALSE", $multiple_cookies="FALSE", $referrer="NONE",
+            $headers=$header, $data=array(), $json_encode="TRUE");
+
+        $url = "https://urlhere";
+
+        $output = $this->shell->parse_exec($opts, $url);
+    }
+?>
